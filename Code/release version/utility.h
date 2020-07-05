e46bcd0cf23502a193209ef611cd68f8582daa77
LoicFournier <67857528+LoicFournier@users.noreply.github.com>
2020-07-05 11:03:24 +0200

    Update utility.h
    Même chose que pour le .ino j'ai modifier les noms de certains états pour être plus cohérents

--- Code/release version/utility.h
+++ Code/release version/utility.h
@@ -11,17 +11,14 @@ typedef enum {
   CAPSULE_AVAILABLE,
   CAPSULE_UNAVAILABLE,
   READ_RFID,
-  RFID_RECOGNIZED,
   CHECK_MONEY_SUFFICIENT,
   MONEY_SUFFICIENT,
   MONEY_INSUFFICIENT,
   CONFIRMATION_PAYMENT,
   PAYMENT,
-  CATCHING_CAPSULE,
-  CAPSULE_CATCHED,
   DROPPING_CAPSULE,
   CAPSULE_DROPPED,
-  COLOR_LED
+  TURN_OFF_LED
 } eState;
 //-------------------------------------------------------------------------------------------
 #endif /* _UTILITY_H_ */
